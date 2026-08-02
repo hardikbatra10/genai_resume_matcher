e86998073f608a30e30ee6ca98cdf7fdbd31a73d
hardikbatra10 <hardik5batra@gmail.com>
2026-08-02 17:15:39 +0530

    fixed CORS on main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://resume-match-point.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
